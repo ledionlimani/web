39bdc936d98564a0ce62643ee45cd500c07d6f1a
ledionlimani <limaniledion501@gmail.com>
2026-03-24 19:19:24 +0100

    module 26

---
+++ module 26/main.js
@@ -0,0 +1,40 @@
+console.log("Hello ")
+
+ var a = 10=24*10
+
+ console .log(a)
+
+ var-67
+ var b=1123
+ var c=a+b
+ console.log(c)
+
+ vara-67
+ var b=1123
+ var c=a+b
+ console.log(k)
+
+ vara-67
+ var b=1123
+ var c=a+b
+ console.log(u)
+
+ vara-67
+ var b=1123
+ var c=a+b
+ console.log(h)
+
+ vara-67
+ var b=1123
+ var c=a+b
+ console.log(c)
+
+ var a=10;
+ var b=20;
+ var c+a+b;
+
+
+ var y=99
+ var
+
+ 
