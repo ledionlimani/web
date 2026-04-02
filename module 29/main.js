7eb09f4ea4a8314e54f170fd873b5ed81185068e
ledionlimani <limaniledion501@gmail.com>
2026-04-02 19:05:43 +0200

    module 29

---
+++ module 29/main.js
@@ -0,0 +1,27 @@
+var program=['java','main.js','nigga'];
+    console.log(program)
+    console.log|(program[2])
+
+
+    program.push('main.js');
+    console.log(program);
+
+    program.pop();
+    console.log(program);
+   
+  console.log(Math.random()*10);
+
+
+console.log(Math.floor(Math.random()*5));
+
+var students = ["john",'drin','filan'];
+
+
+var [s1,s2,s3] = students;
+
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+var numbers=[1,2,3,4,5,6,7,8,9,]
